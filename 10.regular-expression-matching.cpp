e1a61eb6dbb058d8ca3a25432a5dac65dbd7ce53
kody <2270493988@qq.com>
2019-12-17 23:32:30 +0800

    v2

--- 10.regular-expression-matching.cpp
+++ 10.regular-expression-matching.cpp
@@ -28,56 +28,36 @@ false
 Expected Answer
 true
 */
-//https://leetcode.com/problems/regular-expression-matching/discuss/5651/Easy-DP-Java-Solution-with-detailed-Explanation
+//[recommend] The 1st comment on  https://leetcode.com/problems/regular-expression-matching/discuss/5651/Easy-DP-Java-Solution-with-detailed-Explanation
 //https://leetcode.com/problems/regular-expression-matching/discuss/280588/DP-solution-with-the-most-general-natural-explanation
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        if(s.empty() && p.empty()) return true;//
-        vector<vector<bool>>dp(s.size(),vector<bool>(p.size(),false));
-        //初始条件
-        for(int i=0;i<s.size();i++){
-            for(int j=0;j<2;j++){
-                dp[i][j]
+        if(s.empty() && p.empty()) return true;
+        //dp[i][j] represents if s[0,1,...,i-1] can match p[0,1,...,j-1]
+        vector<vector<bool>>dp(s.size()+1,vector<bool>(p.size()+1,false));
+
+        dp[0][0] = true;
+        for(int j=2; j <p.size()+1; j +=2){
+            if(p[j-1] == '*' && dp[0][j-2]){
+                dp[0][j] = true;
             }
         }
-        for(int j=1;j<p.size();j++){
-            if(p[j]==s[0] && p[j-1]=='*') dp[0][j]=true; 
-            if(p[j]=='*') dp[0][j]=dp[0][j-1];
-        }
-        //
-        
-        
-        dp[0][0] = (p[0]==s[0] || p[0]=='.') ? true : false;
-        for(int j=1;j<p.size();j++){
-            if(p[j]==s[0] && p[j-1]=='*') dp[0][j]=true; 
-            if(p[j]=='*') dp[0][j]=dp[0][j-1];
-        }
-        if(p.size()>1)
-            dp[0][1] = (p[1]=='*' || p[1]==NULL) ? dp[0][0] : false;
 
-        for(int i=0;i<s.size();i++){
-            if(p[1]=='*')
-            dp[i][1]
-        }
-        if(s.size()>1 && p.size()>1){
-            dp[1][0]=false;
-            if(p[1]==s[1] || p[1]=='.') dp[1][1]=dp[0][0];
-            if(p[1]=='*' && (p[0]==s[1] || p[0]=='.')) dp[1][1]=dp[0][0];
-        }
-
-
-        for(int i=0;i<s.size();i++){
-            for(int j=0;j<p.size();j++){
-                if(p[j]==s[i] || p[j]=='.') dp[i+1][j+1]=dp[i][j];
-                if(p[j]=='*'){
-                    if(p[j-1]!=s[i] && p[j-1]!='.') dp[i+1][j+1]=dp[i+1][j-1];
-                    if(p[j-1]==s[i] || p[j-1]=='.')
-                        dp[i][j]= dp[i][j-1] || dp[i][j-2] || dp[i-1][j];
+        for(int i=1;i<s.size()+1;i++){
+            for(int j=1;j<p.size()+1;j++){
+                if(p[j-1]==s[i-1] || p[j-1]=='.') dp[i][j]=dp[i-1][j-1];
+                if(p[j-1]=='*'){
+                    if(p[j-2]!=s[i-1] && p[j-2]!='.') 
+                        //repeated 0 times
+                        dp[i][j]=dp[i][j-2];
+                    else 
+                        //repeated 0 times || repeated 1 times || repeated many times
+                        dp[i][j]= dp[i][j-2] || dp[i-1][j-2] || dp[i-1][j];
                 }
             }
         }
-        return dp[s.size()-1][p.size()-1];
+        return dp[s.size()][p.size()];
     }
 };
 // @lc code=end
